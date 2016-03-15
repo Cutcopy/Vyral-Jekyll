57f92362843633ac8603738c81b4a35ad0af1f4f
Bryce Stradling <bryce@getvyral.com>
2016-03-15 16:36:07 -0500

    adjusting header, added omaha skyline pic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("congrats, I'm loaded.");
+// Navbar scroll transition
+$(function(){
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop <= 450){
+      $("#top-nav").removeClass("nav-inverse");
+      //$("#topnav-logo").addClass("hidden");
+    }else{
+      $("#top-nav").addClass("nav-inverse");
+      //$("#topnav-logo").removeClass("hidden");
+    }//if-else
+  });//win func.
+});//ready func.
